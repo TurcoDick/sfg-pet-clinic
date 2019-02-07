2d75971875a996546007a7d827c0c37d177cf815
Alison <alsturco@gmail.com>
2019-02-07 14:03:55 -0200

    Adicionando POJOs. Closes #2.

---
+++ src/main/java/com/alsinteligence/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package com.alsinteligence.sfgpetclinic.model;
+
+public class Person {
+
+    private String fisrtName;
+    private String lasName;
+
+    public String getFisrtName() {
+        return fisrtName;
+    }
+
+    public void setFisrtName(String fisrtName) {
+        this.fisrtName = fisrtName;
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+}
