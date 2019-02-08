60fdd3cf3794d74f578771fc02363adc555e6fcb
Alison <alsturco@gmail.com>
2019-02-08 15:38:19 -0200

    correcting commit, deleting interfaces.

--- pet-clinic-data/src/main/java/com/alsinteligence/sfgpetclinic/services/PersonService.java
+++
@@ -1,6 +0,0 @@
-package com.alsinteligence.sfgpetclinic.services;
-
-import com.alsinteligence.sfgpetclinic.model.Person;
-
-public interface PersonService extends CrudService<Person, Long>{
-}
--- pet-clinic-data/src/main/java/com/alsinteligence/sfgpetclinic/services/PetTypeService.java
+++
@@ -1,7 +0,0 @@
-package com.alsinteligence.sfgpetclinic.services;
-
-import com.alsinteligence.sfgpetclinic.model.PetType;
-
-public interface PetTypeService extends CrudService<PetType, Long>{
-
-}
